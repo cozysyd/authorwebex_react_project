f58ad27cd0cc59ea54c039f07a6f47f35837ba72
Sydney Spaulding <cozysydmedia@gmail.com>
2024-01-28 13:04:59 -0800

    added stylesheet

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import NavBar from './NavBar';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
