14af2cdbf4a61c125be876bdd96febec9be8cf1b
Sydney Spaulding <cozysydmedia@gmail.com>
2024-02-06 02:12:52 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './NavBar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import SeriesOne from './Pages/SeriesOne';
+import ReleaseDate from './Pages/ReleaseDate';
 
 function App() {
+  let component
+    switch (window.location.pathname) {
+      case "/Home":
+        component = <Home />;
+        break;
+      case "/about":
+        component = <About />;
+        break;
+      case "/seriesone":
+        component = <SeriesOne />;
+        break;
+      case "/releasedate":
+        component = <ReleaseDate />;
+        break;
+    }
   return (
     <div>
       <NavBar />
-    </div>
+    </div> 
   );
 }
 
