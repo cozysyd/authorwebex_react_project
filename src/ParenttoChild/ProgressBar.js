9934bce6cbfcb6f7f13ce435a96cf41dea341dde
Sydney Spaulding <cozysydmedia@gmail.com>
2024-02-05 23:03:12 -0800

    debugged progess bar

--- src/ParenttoChild/ProgressBar.js
+++ src/ParenttoChild/ProgressBar.js
@@ -4,7 +4,7 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 
 function ReleaseDate() {
   const now = 60;
-  return <ProgressBar now={now} label={`{now} Days Left!`} />;
+  return <ProgressBar now={now} label={`${now} Days Left!`} />;
 }
 
 export default ReleaseDate;
