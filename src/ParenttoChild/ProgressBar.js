6baca8cf0b0f64ecfa95ae6008ebb367c35e4fd7
Sydney Spaulding <cozysydmedia@gmail.com>
2024-02-05 23:42:59 -0800

    edited progress bar

--- src/ParenttoChild/ProgressBar.js
+++ src/ParenttoChild/ProgressBar.js
@@ -3,6 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 function ReleaseDate() {
+  return (
+    <div>
+      <h1>Release Date Countdown!</h1>
+      <CountdownBar />
+    </div>
+  );
+}
+
+function CountdownBar() {
   const now = 60;
   return <ProgressBar animated now={now} label={`${now} Days Left!`} />;
 }
