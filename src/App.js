9d6ec936eb64f45d48a451501ed8c83326ce015f
Sydney Spaulding <cozysydmedia@gmail.com>
2024-02-05 23:01:59 -0800

    added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SeriesList from './ParenttoChild/SeriesList';
+import ReleaseDate from './ParenttoChild/ProgressBar';
 
 function App() {
   return (
     <div>
       <SeriesList />
+
+      <ReleaseDate />
     </div>
   );
 }
